41e923e5c3b30c9ab585739a357a32631b189631
Yusaku Nishi <jrsaruo@gmail.com>
2017-12-19 22:39:15 +0900

    update DefaultsKey's uniqueValue
    set rawValue as default uniqueValue

--- SafeDefaultsKit/SafeDefaults.swift
+++ SafeDefaultsKit/SafeDefaults.swift
@@ -11,7 +11,16 @@ import Foundation
 /// A type that can be used as a key for SafeDefaults.
 public protocol DefaultsKey: RawRepresentable where Self.RawValue == String {
     /// The unique value used as key for UserDefaults. This value should be entirely unique in the module.
+    /// Default is raw value.
     var uniqueValue: String { get }
+}
+
+public extension DefaultsKey {
+    
+    var uniqueValue: String {
+        return self.rawValue
+    }
+    
 }
 
 // MARK: - SafeDefaults -
