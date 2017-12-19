eddb68f6e690e1befd30834750daaf158ff3b5de
Yusaku Nishi <jrsaruo@gmail.com>
2017-12-19 22:38:29 +0900

    add DefaultsKey and SafeDefaults

--- SafeDefaultsKit/SafeDefaults.swift
+++ SafeDefaultsKit/SafeDefaults.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+
+// MARK: - DefaultsKey -
+/// A type that can be used as a key for SafeDefaults.
+public protocol DefaultsKey: RawRepresentable where Self.RawValue == String {
+    /// The unique value used as key for UserDefaults. This value should be entirely unique in the module.
+    var uniqueValue: String { get }
+}
+
+// MARK: - SafeDefaults -
+/// A type that enables safe access to UserDefaults.
+public protocol SafeDefaults {
+    /// Key set for SafeDefaults.
+    associatedtype Keys: DefaultsKey
+}
