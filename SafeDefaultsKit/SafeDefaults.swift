319586800dd97e2fa045e601f0b85ec63e039d9a
Yusaku Nishi <jrsaruo@gmail.com>
2017-12-19 22:07:53 +0900

    first commit

---
+++ SafeDefaultsKit/SafeDefaults.swift
@@ -0,0 +1,8 @@
+//
+//  SafeDefaults.swift
+//  SafeDefaultsKit
+//
+//  Created by Yusaku Nishi on 2017/12/19.
+//
+
+import Foundation
---
+++ SafeDefaultsKit/SafeDefaultsKit.h
@@ -0,0 +1,18 @@
+//
+//  SafeDefaultsKit.h
+//  SafeDefaultsKit
+//
+//  Created by Yusaku Nishi on 2017/12/19.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SafeDefaultsKit.
+FOUNDATION_EXPORT double SafeDefaultsKitVersionNumber;
+
+//! Project version string for SafeDefaultsKit.
+FOUNDATION_EXPORT const unsigned char SafeDefaultsKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SafeDefaultsKit/PublicHeader.h>
+
+
